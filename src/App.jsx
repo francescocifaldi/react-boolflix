58a47905219f8c776b752b068e14a284665cedbb
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-12 14:25:47 +0100

    ADD try to fetch depends on submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import GlobalContext from './contexts/GlobalContext'
 import { BASE_URI, MY_TOKEN } from './config'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import Content from './pages/Content'
+import axios from 'axios'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([])
+
+  function fetchResult(parm) {
+    axios.get(`${BASE_URI}search/movie?api_key=${MY_TOKEN}&query=${parm}`)
+      .then(res => {
+        console.log(res.data.results)
+        setList(res.data.results)
+      })
+  }
+
+  useEffect(() => {
+    fetchResult()
+  }, [])
 
   return (
     <>
-      <GlobalContext.Provider>
+      <GlobalContext.Provider value={{ list, setList, fetchResult }}>
         <BrowserRouter>
           <Routes>
             <Route element={<DefaultLayout />}>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,10 +1,28 @@
+import { useContext } from "react"
+import GlobalContext from "../contexts/GlobalContext"
+import { useState } from "react"
+
 export default function Searchbar() {
+    const [search, setSearch] = useState('')
+    const { setList, fetchResult } = useContext(GlobalContext)
+
+    function onSubmit(e) {
+        e.preventDefault()
+        fetchResult(search)
+    }
+
+    function onChange(e) {
+        setSearch(e.target.value)
+        console.log(search)
+    }
+
     return (
         <div>
             sono la searchbar
-            <form>
-                <input type="text" />
-                <button>Vai</button>
+            <form onSubmit={onSubmit} action="" className='form'>
+                <label htmlFor="search">Titolo</label>
+                <input onChange={onChange} id='title' name='title' value={search} type="text" placeholder='Contenuto da cercare' />
+                <input type="submit" value='VAI' />
             </form>
         </div>
     )
