3c02a2f3b96094324226866ae65af9bd349690e1
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-12 12:03:48 +0100

    ADD routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import GlobalContext from './contexts/GlobalContext'
 import { BASE_URI, MY_TOKEN } from './config'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import DefaultLayout from './layouts/DefaultLayout'
+import Content from './pages/Content'
 
 
 function App() {
@@ -9,9 +12,13 @@ function App() {
   return (
     <>
       <GlobalContext.Provider>
-        <div>
-          CIAO
-        </div>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<Content />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </GlobalContext.Provider>
     </>
   )
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,7 @@
 export default function Searchbar() {
     return (
         <div>
+            sono la searchbar
             <form>
                 <input type="text" />
                 <button>Vai</button>
