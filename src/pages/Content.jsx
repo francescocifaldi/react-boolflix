d44e32202727b304b0bc28d85959ba150f87b38d
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-12 12:15:20 +0100

    ADD try query with "ritorno"

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react"
+import { BASE_URI, MY_TOKEN } from "../config"
+import axios from "axios"
+
 export default function Content() {
+    const [list, setList] = useState([])
+
+    function fetchResult() {
+        axios.get(`${BASE_URI}search/movie?api_key=${MY_TOKEN}&query=ritorno`)
+            .then(res => {
+                console.log(res.data.results)
+                setList(res.data.results)
+            })
+    }
+
+    useEffect(() => {
+        fetchResult()
+    }, [])
+
     return (
         <>
             <div>sono il contenuto</div>
