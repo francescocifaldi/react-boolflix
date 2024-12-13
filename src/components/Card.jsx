25af2a43d0a29d6441e70ac64846d30cd26d920a
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-13 16:52:29 +0100

    ADD fontawesome to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,30 @@
 import flagsMap from "../utils/flagsMap"
 import { IMG_BASE_URL } from "../utils/config"
 const imgDimension = 'w185'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from "@fortawesome/free-solid-svg-icons/faStar"
 
 export default function Card({ content }) {
     const { title, original_title, original_language, vote_average, poster_path } = content
 
     return (
-        <div className='debug'>
+        <>
+            <div className='debug'>
 
-            <h2>{title}</h2>
+                <h2>{title}</h2>
 
-            <p>Titolo originale:{original_title}</p>
+                <p>Titolo originale:{original_title}</p>
 
-            {flagsMap[original_language] ? <img src={flagsMap[original_language]} height={20} /> : <p>{original_language}</p>}
+                {flagsMap[original_language] ? <img src={flagsMap[original_language]} height={20} /> : <p>{original_language}</p>}
 
-            <p>Voto:{vote_average}</p>
+                <p>Voto:{Math.ceil((vote_average) / 2)}</p>
 
-            <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" />
+                <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" />
 
-        </div>
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faStar} />
+            </div>
+        </>
     )
 }
