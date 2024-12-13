ae152989ccf12388fc97a5eef0e5f004baf58786
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-13 19:00:30 +0100

    ADD hover effect, working on fix padding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons/faStar"
 import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons"
 import style from './Card.module.css'
+import { useState } from "react"
 
 export default function Card({ content }) {
     const { title, original_title, original_language, vote_average, poster_path } = content
+    const [hover, setHover] = useState(false);
 
     const starArr = [];
     for (let i = 0; i < 5; i++) {
@@ -18,18 +20,18 @@ export default function Card({ content }) {
         }
     }
 
-    /*            
+    const poster = <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" className={style.cover} />
+    const description = <div className={style.description}>
         <h2>{title}</h2>
         <p>Titolo originale:{original_title}</p>
         {flagsMap[original_language] ? <img src={flagsMap[original_language]} height={20} /> : <p>{original_language}</p>}
         <p>{starArr}</p>
-    */
+    </div>
 
     return (
-        <div className={`debug ${style.card}`}>
-
-
-            <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" className={style.cover} />
+        <div className={`debug ${style.card}`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+            {hover && description}
+            {poster}
         </div >
     )
 }
