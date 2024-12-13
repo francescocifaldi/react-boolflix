4cc6019a12c5105fec9535943e568e0830b9eb60
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-13 17:19:20 +0100

    ADD stars handling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,35 @@
 import flagsMap from "../utils/flagsMap"
 import { IMG_BASE_URL } from "../utils/config"
-const imgDimension = 'w185'
+const imgDimension = 'w342'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from "@fortawesome/free-solid-svg-icons/faStar"
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons/faStar"
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons"
 
 export default function Card({ content }) {
     const { title, original_title, original_language, vote_average, poster_path } = content
 
+    const starArr = [];
+    for (let i = 0; i < 5; i++) {
+        if (i < Math.ceil((vote_average) / 2)) {
+            starArr.push(<FontAwesomeIcon key={i} icon={fullStar} />);
+        } else {
+            starArr.push(<FontAwesomeIcon key={i} icon={emptyStar} />);
+        }
+    }
+
+
     return (
-        <>
-            <div className='debug'>
+        <div className='debug'>
+            <h2>{title}</h2>
 
-                <h2>{title}</h2>
+            <p>Titolo originale:{original_title}</p>
 
-                <p>Titolo originale:{original_title}</p>
+            {flagsMap[original_language] ? <img src={flagsMap[original_language]} height={20} /> : <p>{original_language}</p>}
 
-                {flagsMap[original_language] ? <img src={flagsMap[original_language]} height={20} /> : <p>{original_language}</p>}
+            <p>{starArr}</p>
 
-                <p>Voto:{Math.ceil((vote_average) / 2)}</p>
+            <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" />
 
-                <img src={`${IMG_BASE_URL}${imgDimension}${poster_path}`} alt="" />
-
-            </div>
-            <div>
-                <FontAwesomeIcon icon={faStar} />
-            </div>
-        </>
+        </div>
     )
 }
